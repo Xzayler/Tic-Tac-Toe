854d87893081c7fe7969a1a26a78844c1511d69b
Xzayler <csak.mityu@gmail.com>
2023-01-12 20:40:39 +0100

    Add reset to gameflow module to reset the grid

--- script.js
+++ script.js
@@ -13,15 +13,25 @@ const gameboard = (() => {
 })();
 
 const gameflow = (() => {
-  // true signifies
+  // true signifies crosses and flase is naughts
   var turn = true;
   const setMove = (e) => {
     e.target.classList.add(turn ? 'cross' : 'naught');
     turn = !turn;
   };
 
+  // Button to reset
+  const reset = () => {
+    cells.forEach((cell) => {
+      cell.classList = 'cell';
+    });
+    //set turn to 'x' player
+    turn = true;
+  };
+
   return {
     setMove,
+    reset,
   };
 })();
 
@@ -30,3 +40,4 @@ cells = document.querySelectorAll('.cell');
 cells.forEach((cell) => {
   cell.addEventListener('click', (e) => gameflow.setMove(e));
 });
+document.querySelector('.reset').addEventListener('click', gameflow.reset);
